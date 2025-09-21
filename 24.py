c8f83db10f1f49135536e7189a6f62e2922814f5
Sharada24git <sharushambhavi24@gmail.com>
2025-09-21 19:38:26 +0530

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,9 @@
+n1 = 3
+n2 = 4
+sum = n1+n2
+dif = n1-n2
+exp = n1**n2
+div1 = n1/n2
+div2 = n1//n2
+multi = n1*n2
+print(sum,dif,exp,div1,div2,multi)
---
+++ 26.py
@@ -0,0 +1,8 @@
+for j in range(0,12,1):
+    for i in range(0,12,1):
+        time1=str(9+j)+":"+str(5*i).zfill(2)
+        angle1=(90-30*j)+i*30-i*2.5
+        print(time1,"-",angle1%360)
+    print()
+
+
